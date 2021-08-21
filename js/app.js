b264ad84572858006b14baeb310f670997bd03f5
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-08-21 06:31:57 -0600

    finished createing the nav mobile view

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 console.log("hello world!!")
+
+
+// this code is for the navlinks -->it makes sure there is some kind of hover or click event on the nav
+const navlinks= document.querySelector(".navlinks");
+//this just mimics hoverstate on laptop but in a Mobilerstate
+navlinks.addEventListener('click',(e)=>{
+    // console.log((e.target.className))
+    if(e.target.parentElement.className ==='navlink'){
+       
+        e.target.classList.add("a_tag_hover_state")
+
+        setTimeout(()=>{
+        e.target.classList.remove("a_tag_hover_state")
+
+        },1000)
+
+    }
+    })
